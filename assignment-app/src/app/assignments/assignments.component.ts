0a09ff00a921abd3d1660277bca80d1e566a1bae
SNAYZZ72 <103134452+SNAYZZ72@users.noreply.github.com>
2024-01-29 11:53:48 +0100

    third commit

--- assignment-app/src/app/assignments/assignments.component.ts
+++ assignment-app/src/app/assignments/assignments.component.ts
@@ -1,10 +1,11 @@
 import { Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RenduDirective } from '../../shared/rendu.directive';
 
 @Component({
   selector: 'app-assignments',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RenduDirective],
   templateUrl: './assignments.component.html',
   styleUrl: './assignments.component.css'
 })
