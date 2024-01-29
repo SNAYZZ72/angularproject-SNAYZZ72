5cdab939d85118e248401e25968717a3f158ff3a
SNAYZZ72 <103134452+SNAYZZ72@users.noreply.github.com>
2024-01-29 17:30:05 +0100

    rendu 1  commit

--- assignment-app/src/app/app.component.ts
+++ assignment-app/src/app/app.component.ts
@@ -1,19 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { AssignmentsComponent } from './assignments/assignments.component';
+import {MatToolbar, MatToolbarRow} from "@angular/material/toolbar";
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
+
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule, MatIconModule, MatDividerModule, AssignmentsComponent],
+  imports: [RouterOutlet, MatButtonModule, MatIconModule, MatDividerModule, AssignmentsComponent, MatToolbar, MatSidenavModule, MatListModule, MatToolbarRow],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'assignment-app';
+  opened: boolean = false;
 }
