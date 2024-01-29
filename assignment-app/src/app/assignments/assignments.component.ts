8eb0aecd14797fbecfa5448d462dd152689ff6e7
SNAYZZ72 <103134452+SNAYZZ72@users.noreply.github.com>
2024-01-29 14:49:05 +0100

    four commit

--- assignment-app/src/app/assignments/assignments.component.ts
+++ assignment-app/src/app/assignments/assignments.component.ts
@@ -1,16 +1,36 @@
-import { Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RenduDirective } from '../../shared/rendu.directive';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+
 
 @Component({
   selector: 'app-assignments',
   standalone: true,
-  imports: [CommonModule, RenduDirective],
+  imports: [CommonModule, RenduDirective, MatButtonModule, FormsModule, MatIconModule, MatInputModule, MatFormFieldModule, MatDatepickerModule],
   templateUrl: './assignments.component.html',
   styleUrl: './assignments.component.css'
 })
-export class AssignmentsComponent {
+export class AssignmentsComponent implements OnInit {
+
+  ngOnInit(): void {
+    setTimeout( () => {
+      this.ajoutActive = true
+    }, 2000);
+  }
+
+  onSubmit() {
+    console.log(this.nomDevoir)
+  }
+
   titre = 'mon application sur les assignments'
+  ajoutActive = false
+  nomDevoir = ''
   assignments = [
     {
       nom: 'TP Angular 1',
@@ -28,5 +48,6 @@ export class AssignmentsComponent {
       rendu: false
     }
   ]
+
 
 }
